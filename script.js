6a978d723addb60699fccc664afa5fb704a93d86
Baniyo <Aw200946@icloud.com>
2026-03-05 20:29:59 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+//ボタンで階を切り替える
+function showScreen(id){
+  const screens=document.querySelectorAll('.screen');
+  screens.forEach(screen => screen.classList.remove('active'));
+  document.getElementById(id).classList.add('active');
+}
+//SVGの部屋をクリックしたら色が赤色になる
+const obj=document.getElementById("map_1f");
+obj.addEventListener("load",function(){
+  const svgDoc=obj.contentDocument;
+  const rooms=svgDoc.querySelectorAll("rect,path");
+
+  rooms.forEach(room=>{
+    room.dataset.originalFill=room.style.fill;
+    room.addEventListener("click",function(){
+      if(room.style.fill==="red"){
+        room.style.fill=room.dataset.originalFill;
+      }else{
+        room.style.fill="red";
+      }
+    });
+  });
+});
+const obj2=document.getElementById("map_2f");
+obj2.addEventListener("load",function(){
+  const svgDoc=obj2.contentDocument;
+  const rooms=svgDoc.querySelectorAll("rect,path");
+
+  rooms.forEach(room=>{
+    room.dataset.originalFill=room.style.fill;
+    room.addEventListener("click",function(){
+      if(room.style.fill==="red"){
+        room.style.fill=room.dataset.originalFill;
+      }else{
+        room.style.fill="red";
+      }
+    });
+  });
+});
+const obj3=document.getElementById("map_3f");
+obj3.addEventListener("load",function(){
+  const svgDoc=obj3.contentDocument;
+  const rooms=svgDoc.querySelectorAll("rect,path");
+  rooms.forEach(room=>{
+    room.dataset.originalFill=room.style.fill;
+    room.addEventListener("click",function(){
+      if(room.style.fill==="red"){
+        room.style.fill=room.dataset.originalFill;
+      }else{
+        room.style.fill="red";
+      }
+    });
+  });
+});
