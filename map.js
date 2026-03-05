a10815538d3d6a1ab9cdb297662f462e58574709
Baniyo <Aw200946@icloud.com>
2026-03-05 10:27:03 +0900

    Add files via upload

--- map.js
+++ map.js
@@ -1,5 +1,21 @@
-function showScreen(id) {
-  const screens = document.querySelectorAll('.screen');
+//ボタンで階を切り替える
+function showScreen(id){
+  const screens=document.querySelectorAll('.screen');
   screens.forEach(screen => screen.classList.remove('active'));
   document.getElementById(id).classList.add('active');
 }
+//SVGの部屋をクリックしたら色が赤色になる
+const obj=document.getElementById("map_2f");
+obj.addEventListener("load",function(){
+  const svgDoc=obj.contentDocument;
+  const rooms=svgDoc.querySelectorAll("rect,path");
+  rooms.forEach(room=>{
+    room.addEventListener("click",function(){
+      if(room.style.fill==="red"){
+        room.style.fill="";
+      }else{
+         room.style.fill = "red";
+      }
+    });
+  });
+});
